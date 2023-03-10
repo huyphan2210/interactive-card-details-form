7c86b9298258f68a6cb3c684351506c13a7ca0d2
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-10 09:11:34 +0700

    Prevent users from entering & pasting invalid characters

--- function.js
+++ function.js
@@ -1,6 +1,100 @@
 $(document).ready(function() {
-    $('#number').on('change keyup', function() {
-        $(this).val(cc_format($(this).val()));
+    // Prevent users from entering and pasting numbers.
+    $('#name').on('keydown paste', function(e) {
+        e = e || window.event;
+        if (e.type === 'paste') {
+            const pastedData = e.originalEvent.clipboardData.getData('text');
+            if (!/^[a-zA-Z ]+$/.test(pastedData)) {
+                return false;
+            }
+        } else {
+            const charCode = (typeof e.which === "undefined") ? e.keyCode : e.which;
+            const charStr = String.fromCharCode(charCode);
+            if (
+                !/([A-Z]|[a-z]| )/.test(charStr) &&
+                charCode !== 8 &&
+                charCode !== 46
+            ) {
+                return false;
+            }    
+        }
+    })
+
+    // Prevent users from entering and pasting characters that are not numbers.
+    $('#number').on('keydown paste', function(e) {
+        e = e || window.event;
+        if (e.type === 'paste') {
+            const pastedData = e.originalEvent.clipboardData.getData('text');
+            if (!/^[0-9 ]+$/.test(pastedData)) {
+                return false;
+            }
+        } else {
+            const charCode = (typeof e.which === "undefined") ? e.keyCode : e.which;
+            const charStr = String.fromCharCode(charCode);
+            if (
+                !/([0-9]| )/.test(charStr) &&
+                charCode !== 8 &&
+                charCode !== 46
+            ) {
+                return false;
+            } 
+        }
+
+        const previousVal = $(this).val();
+
+        $(this).val(cc_format(previousVal));
+
+        if (previousVal === '') {
+            $('.number').text('0000 0000 0000 0000');
+        } else {
+            $('.number').text(cc_format(previousVal));
+        }
+    })
+
+    $('.dateAndCvc input').on('keydown paste', function(e) {
+        e = e || window.event;
+        if (e.type === 'paste') {
+            const pastedData = e.originalEvent.clipboardData.getData('text');
+            if (!/^[0-9]+$/.test(pastedData)) {
+                return false;
+            }
+        } else {
+            const charCode = (typeof e.which === "undefined") ? e.keyCode : e.which;
+            const charStr = String.fromCharCode(charCode);
+            if (
+                !/([0-9])/.test(charStr) &&
+                charCode !== 8 &&
+                charCode !== 46
+            ) {
+                return false;
+            }      
+        }
+    })
+
+    $('#name, .dateAndCvc input').keyup(function() {
+        if (!$('#name').val()) {
+            $('.holder .name').text('Jane Appleseed')
+        } else {
+            $('.holder .name').text($('#name').val());
+        }
+
+        if (!$('#expDate input:first-child').val()) {
+            $('.holder div span:first-child').text('00');
+        } else {
+            $('.holder div span:first-child').text($('#expDate input:first-child').val());
+        }
+
+        if (!$('#expDate input:last-child').val()) {
+            $('.holder div span:last-child').text('00');
+        } else {
+            $('.holder div span:last-child').text($('#expDate input:last-child').val());
+        }
+        console.log($('.cvcSection input').val())
+        if (!$('.cvcSection input').val()) {
+            $('.cardBack span').text('000');
+        } else {
+            $('.cardBack span').text($('.cvcSection input').val());
+        }
     })
     
     $('form').submit(function(e) {
